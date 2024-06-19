bb02f18d055fe7b78b8635f8ef9f0949799b105f
Yanan <Evazhao919@gmail.com>
2024-06-19 17:30:56 -0500

    All the test about delete founction down

--- CareCompassLambda/src/test/java/com/devyanan/CareCompass/activity/DeleteMedicationActivityTest.java
+++ CareCompassLambda/src/test/java/com/devyanan/CareCompass/activity/DeleteMedicationActivityTest.java
@@ -1,4 +1,54 @@
 package com.devyanan.CareCompass.activity;
 
+import com.devyanan.CareCompass.activity.requests.DeleteMedicationRequest;
+import com.devyanan.CareCompass.activity.results.DeleteMedicationResult;
+import com.devyanan.CareCompass.dynamodb.MedicationDao;
+import com.devyanan.CareCompass.dynamodb.models.Medication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
 public class DeleteMedicationActivityTest {
+    @Mock
+    private MedicationDao medicationDao;
+
+    private DeleteMedicationActivity deleteMedicationActivity;
+
+    @BeforeEach
+    public void setUp() {
+        openMocks(this);
+        deleteMedicationActivity = new DeleteMedicationActivity(medicationDao);
+    }
+
+    @Test
+    public void handleRequest_withValidPatientIdAndMedicationId_removesAndReturnsMedication() {
+        // Given
+        String patientId = "patient123";
+        String medicationId =  "2023-06-19T16:51";
+
+        Medication medicationToDelete = new Medication();
+        medicationToDelete.setPatientId(patientId);
+        medicationToDelete.setMedicationId(medicationId);
+
+        when(medicationDao.deleteMedication(medicationToDelete))
+                .thenReturn(medicationToDelete);
+
+        // When
+        DeleteMedicationRequest request = DeleteMedicationRequest.builder()
+                .withPatientId(patientId)
+                .withMedicationId(medicationId)
+                .build();
+
+        DeleteMedicationResult result = deleteMedicationActivity.handleRequest(request);
+
+        verify(medicationDao).deleteMedication(medicationToDelete);
+
+        assertEquals(patientId, result.getMedicationModel().getPatientId());
+        assertEquals(medicationId, result.getMedicationModel().getMedicationId());
+    }
 }
--- CareCompassLambda/src/test/java/com/devyanan/CareCompass/activity/DeleteVitalSignsActivityTest.java
+++ CareCompassLambda/src/test/java/com/devyanan/CareCompass/activity/DeleteVitalSignsActivityTest.java
@@ -1,4 +1,61 @@
 package com.devyanan.CareCompass.activity;
 
+import com.devyanan.CareCompass.activity.requests.DeleteVitalSignsRequest;
+import com.devyanan.CareCompass.activity.results.DeleteVitalSignsResult;
+import com.devyanan.CareCompass.converters.LocalDateTimeConverter;
+import com.devyanan.CareCompass.dynamodb.VitalSignsDao;
+import com.devyanan.CareCompass.dynamodb.models.VitalSigns;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.openMocks;
+
 public class DeleteVitalSignsActivityTest {
+
+    @Mock
+    private VitalSignsDao vitalSignsDao;
+
+    private DeleteVitalSignsActivity deleteVitalSignsActivity;
+
+    @BeforeEach
+    public void setUp() {
+        openMocks(this);
+        deleteVitalSignsActivity = new DeleteVitalSignsActivity(vitalSignsDao);
+    }
+
+    @Test
+    public void handleRequest_withValidPatientIdAndActualCheckTime_removesAndReturnsVitalSigns() {
+        // Given
+        LocalDateTimeConverter timeConverter = new LocalDateTimeConverter();
+
+        String patientId = "patient123";
+        String actualCheckTime =  "2023-06-19T16:51";
+
+        VitalSigns vitalSignsToDelete = new VitalSigns();
+        vitalSignsToDelete.setPatientId(patientId);
+        vitalSignsToDelete.setActualCheckTime(timeConverter.unconvert(actualCheckTime));
+
+        // Mock behavior
+        when(vitalSignsDao.deleteSingleVitalSignsByActualCheckTime(anyString(), any(LocalDateTime.class)))
+                .thenReturn(vitalSignsToDelete);
+
+        // When
+        DeleteVitalSignsRequest request = DeleteVitalSignsRequest.builder()
+                .withPatientId(patientId)
+                .withActualCheckTime(actualCheckTime)
+                .build();
+
+        DeleteVitalSignsResult result = deleteVitalSignsActivity.handleRequest(request);
+
+        verify(vitalSignsDao).deleteSingleVitalSignsByActualCheckTime(patientId, timeConverter.unconvert(actualCheckTime));
+
+        assertEquals(patientId, result.getVitalSignModel().getPatientId());
+        assertEquals(actualCheckTime, result.getVitalSignModel().getActualCheckTime());
+    }
 }
